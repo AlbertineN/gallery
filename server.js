b99f8b9ecccfda525c6f9d39628d035ef334aa87
AlbertineN <albertinengingan@gmail.com>
2023-04-09 23:24:22 +0300

    Updated the server.js after  merge

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const app = express();
 // connecting the database
 
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    };
+//const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+//mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+//    if (err) {
+//        console.log(err)
+//    }else{
+//        console.log(`Connected to Database: ${MONGODB_URI}`)
+//    };
 
 //let mongodb_url = 'mongodb://localhost:27017/';
 let mongodb_url = 'mongodb+srv://albertine:BbNQU6XupdXeewMC@gallerycluster.leeq3jp.mongodb.net/?retryWrites=true&w=majority';
@@ -30,10 +30,10 @@ mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedT
 });
 
 // test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
+ let db = mongoose.connection;
+ db.once('open', ()=>{
+     console.log('Database connected successfully')
+ });
 
 
 
